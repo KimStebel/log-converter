00abe581a4bc127d6368d8973a74324e751f4153
Kim Stebel <kim.stebel@gmail.com>
2022-03-03 00:24:00 +0000

    first prototype

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,51 @@
-use regex::Regex as R;
+use regex::Regex;
+use json::object;
+use std::io;  
 
-fn main() {
-    let line = "2022-01-01 12:42:34.000Z INFO some message here";
-    let re = R::new(r"^(\d{4})-(\d{2})-(\d{2})").unwrap();
+fn parse(s: String, re: &Regex) -> json::JsonValue {            
+    // if it's json, assume it's good and pass it on
+    match json::parse(&s) {
+        Ok(json) => {return json;}
+        Err(_) => {
+            // if it's not json, try to parse the severity out of it and turn it into json
+            match re.captures_iter(&s).next() {
+                Some(cap) => {
+                    let severity = cap[1].to_uppercase();
+                    return object! {
+                        severity: severity,
+                        message: s.trim(),
+                        timestamp: {
+                            seconds: 0,
+                            nanos: 0     
+                        }
+                    };
+                }
+                None => { // if everything else fails, assume severity is Info and put everything into the message field
+                    return object! {
+                        severity: "INFO",
+                        message: s.trim(),
+                        timestamp: {
+                            seconds: 0,
+                            nanos: 0           
+                        }
+                    };
+                }
+            } 
+        }
+    }
+}
 
-    let cap = re.captures_iter(line).next().unwrap();
-    println!("Month: {} Day: {} Year: {}", &cap[2], &cap[3], &cap[1]);
-    println!("{}", re.is_match(line));
-    println!("end");
+fn main() {    
+    let re = Regex::new(r"(?i)(fatal|error|err|warning|warn|info|debug|trace)").expect("failed to compile regex");
+    loop {
+        let mut input_line = String::new();
+        let bytes_read = io::stdin().read_line(&mut input_line).expect("can't read from stdin, exiting");
+        if bytes_read == 0 { // reached EOF
+            break;
+        }
+        let input_line = input_line;
+        let log_entry = parse(input_line, &re);
+        println!("{}", log_entry);
+
+    }
 }
